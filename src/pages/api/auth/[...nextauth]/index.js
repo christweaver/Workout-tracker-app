fb0001fa32b672f01959719bdc4ecefef4743652
christy <christyweaver.cw@gmail.com>
2024-02-15 17:39:01 -0600

    added base url

--- src/pages/api/auth/[...nextauth]/index.js
+++ src/pages/api/auth/[...nextauth]/index.js
@@ -4,6 +4,7 @@ import connectToDb from "../../../../db/mongodb";
 import User from "../../../../models/userSchema";
 
 const authOptions = {
+  baseUrl: "https://workout-tracker-hlk62yy87-christys-projects.vercel.app", // Replace this with your actual production base URL
   providers: [
     CredentialsProvider({
       type: "credentials",
